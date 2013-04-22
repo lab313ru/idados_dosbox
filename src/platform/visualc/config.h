61019dded05af859773d689f8a0c8b81471b3b71
LowLevelMahn <dl.soluz@gmx.net>
2013-04-22 07:51:53 +0200

    Add vs2010 solution

--- src/platform/visualc/config.h
+++ src/platform/visualc/config.h
@@ -1,7 +1,7 @@
 #define VERSION "SVN"
 
 /* Define to 1 to enable internal debugger, requires libcurses */
-#define C_DEBUG 0
+#define C_DEBUG 1
 
 /* Define to 1 to enable screenshots, requires libpng */
 #define C_SSHOT 1
@@ -16,7 +16,7 @@
 #define C_IPX 1
 
 /* Enable some heavy debugging options */
-#define C_HEAVY_DEBUG 0
+#define C_HEAVY_DEBUG 1
 
 /* The type of cpu this host has */
 #define C_TARGETCPU X86
